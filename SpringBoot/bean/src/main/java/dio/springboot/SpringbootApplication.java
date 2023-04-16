e5c625fac3e043510e0c4d4a592f98dceabaaad0
carol-sf <annacarolina.franca21@gmail.com>
2023-04-16 07:43:34 -0300

    adicionando bens e outras anotações

--- SpringBoot/bean/src/main/java/dio/springboot/SpringbootApplication.java
+++ SpringBoot/bean/src/main/java/dio/springboot/SpringbootApplication.java
@@ -1,13 +1,24 @@
 package dio.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) {
+		return args -> {
+			String json = "{\"cep\": \"01001-000\",\"logradouro\": \"Praça da Sé\",\"localidade\": \"São Paulo\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("\nDados do CEP: " + response + "\n");
+		};
 	}
 
 }
