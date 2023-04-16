a5926f869a2254fba3552de705c5a5421ea637a3
carol-sf <annacarolina.franca21@gmail.com>
2023-04-16 08:44:39 -0300

    tirando todos os projetos spring e fazendo testes

---
+++ spring-boot/teste-springboot/src/main/java/dio/springboot/TesteSpringbootApplication.java
@@ -0,0 +1,13 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSpringbootApplication.class, args);
+	}
+
+}
